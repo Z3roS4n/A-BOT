7479c276fbf9d91e59a586e76b2e98a2fc742096
Antonio Murabito <Thepro007002001@gmail.com>
2021-02-27 18:26:25 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,16 +1,16 @@
 <?php
 
 $settings = [
-    "token" => "1638054048:AAGkxNi7hRhc--YQUu_4i_Uf0_xAvhKjqgQ",
+    "token" => "BOT_TOKEN",
     "mysqli" => false,
     "dev_mode" => false //enable this feature only in limited cases, it will make the bot unusable for normal users!
 ];
 
 $db = [
-    "hostname" => "-", //server address
-    "database" => "-", //name of your database
-    "password" => "-", //database password
-    "username" => "-"
+    "hostname" => "ADDRESS", //server address
+    "database" => "DATABASE", //name of your database
+    "password" => "PASSWORD", //database password
+    "username" => "USERNAME"
 ];
 
 ?>

f7a07f4ac66a48f74784649e546ca7637871de5a
Antonio Murabito <Thepro007002001@gmail.com>
2021-02-27 18:07:47 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -3,7 +3,7 @@
 $settings = [
     "token" => "1638054048:AAGkxNi7hRhc--YQUu_4i_Uf0_xAvhKjqgQ",
     "mysqli" => false,
-    "dev_mode" => true //enable this feature only in limited cases, it will make the bot unusable for normal users!
+    "dev_mode" => false //enable this feature only in limited cases, it will make the bot unusable for normal users!
 ];
 
 $db = [

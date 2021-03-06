b2a0e0822d87105862f4d4064fa990d02d74d152
Antonio Murabito <Thepro007002001@gmail.com>
2021-03-06 15:26:28 +0100

    Update var.php

--- var.php
+++ var.php
@@ -1,15 +1,12 @@
 <?php
 
 $message = $update["message"];
-$MemberStatus = $update['chatmember']['status'];
 $chatId = $message['chat']['id'];
 $name = $message['from']['first_name'];
 $surname = $message['from']['last_name'];
 $text = $message['text'];
 $userID = $message['from']['id'];
 $username = $message['from']['username'];
-$isBot = $message["from"]["is_bot"];
-$dicevalue = $message['dice']['value'];
 
 $msgid = $message["message_id"];
 $replyId = $message["reply_to_message"]["from"]["id"];
@@ -19,6 +16,7 @@ $replyName = $message["reply_to_message"]["from"]["first_name"];
 $replySurname = $message["reply_to_message"]["from"]["last_name"];
 $replyIsbot = $message["reply_to_message"]["from"]["is_bot"];
 $replymsgid = $message["reply_to_message"]["message_id"];
+
 $newmember = $message["new_chat_member"];
 $newmemberid = $message["new_chat_member"]["id"];
 $leftmember = $message["left_chat_member"];
@@ -37,12 +35,12 @@ $queryName = $query['from']['first_name'];
 $querydata = $query['data'];
 $querymsgid = $query['message']['message_id'];
 
-if($username == false)
+if(!$username)
     $username = "<i>NO USERNAME</i>";
 else
     $username = "@$username";
   
-if($name == false)
+if(!$name)
     $name = $queryName;
 else
     $name = $name;
